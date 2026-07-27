82bfa8071a7912dfbd30b498bcd024296f4ef236
Ahmed <ahmedbouhtit8@gmail.com>
2026-07-27 11:58:32 +0100

    finishing ocm

--- lib/services/ocm_service.dart
+++ lib/services/ocm_service.dart
@@ -14,6 +14,14 @@ class OcmService{
        '$_baseUrl?output=json&latitude=$latitude&longitude=$longitude&distance=$distanceKm&maxresults=$maxResults',
     );
     final response= await http.get(url);
+
+    if (response.statusCode==200){
+      final List<dynamic> jsonList= jsonDecode(response.body);
+      return jsonList.map((json) => ChargingStation.fromJson(json)).toList();
+
+    }else{
+      throw Exception('Failed to fetch charging stations: ${response.statusCode}');
+    }
   }
 
 }
